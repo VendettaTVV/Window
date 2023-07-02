fbf4efac5013c918a4dd05de3c10ad8f995d7d21
VendettaTVV <vvtkachukvv@gmail.com>
2023-07-02 12:07:22 +0300

    tabs

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,24 +1,41 @@
 
 
 const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
-const header = document.querySelector(headerSelector),
-tab = document.querySelector(headerSelector),
-content = document.querySelector(contentSelectorSelector);
+    const header = document.querySelector(headerSelector),
+        tab = document.querySelectorAll(tabSelector),
+        content = document.querySelectorAll(contentSelector);
 
-function hideTabContent() {
-    content.forEach(item => {
-        item.style.display = 'none';
-    });
-    tab.forEach(item => {
-        item.classList.remove(activeClass);
-    });
-}
+    function hideTabContent() {
+        content.forEach(item => {
+            item.style.display = 'none';
+        });
 
-function showTabContent(i) {
-    content[i].style.display = 'block';
-    tab[i].classList.add(activeClass);
-}
-}
+        tab.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+    }
+
+    function showTabContent(i = 0) {
+        content[i].style.display = 'block';
+        tab[i].classList.add(activeClass);
+    }
+    hideTabContent();
+    showTabContent();
+
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target &&
+            (target.classList.contains(tabSelector.replace(/\./, "")) ||
+            target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {
+            tab.forEach((item, i) => {
+                if (target == item || target.parentNode == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+};
 
 
 export default tabs;
